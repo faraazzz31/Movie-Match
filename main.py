796f6a6be870f4c29eaa04873e6724e6f96466b9
faraazzz31 <118736125+faraazzz31@users.noreply.github.com>
2023-03-26 19:41:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,116 @@
-def cosine_similarity(m1: list[int], m2: list[int]) -> int:
-  """Returns the cosine similarity value between two lists.
-  
-  Preconditions:
-  - len(m1) == len(m2)
-  """
-  dot = sum([m1[i] * m2[i] for i in range(0, len(m1))])
-  n1 = sum([m1[i] ** 2 for i in range(0, len(m1))]) ** 0.5
-  n2 = sum([m2[i] ** 2 for i in range(0, len(m2))]) ** 0.5
-  
-  return dot / (n1 * n2) 
+"""
+CSC11 Course Project: Movie Match (Movie Recommendation System)
+Authors: Christoffer Tan, Faraaz Ahmed, Razan Rifandi
+"""
 
-if __name__ == '__main__':
-  print("Hello World")
-  for i in range(0, 5):
-    print(i)
+from __future__ import annotations
+import csv
+import pandas
+from typing import Optional
+
+#TODO: add pythonta and pytest
+
+movie_title_mapping = {} #mapping of movie title to Movie Vertex
+
+class User:
+    """
+    User class where each user acts as a Vertex in the graph.
+
+    Instance Attributes:
+    - user_id:
+        The unique id of the user.
+    - movies:
+        The mapping of movies the user has watched where all keys are the movies and the corresponding values are
+        the ratings.
+
+    Representation Invariants:
+    ...
+    """
+    user_id: int
+    movies: dict[Movie, float]
+
+    def __init__(self, user_id: int):
+        self.user_id = user_id
+
+
+class Movie:
+    """
+        Movie class where each movie acts as a Vertex in the graph.
+
+        Instance Attributes:
+        - movie_id:
+            The unique id of the user.
+        - users:
+            The mapping of users who have watched the movie where all keys are the users and the corresponding values
+            are the ratings provided by the users.
+        - title:
+            The name of the movie.
+
+        Representation Invariants:
+        ...
+        """
+    movie_id: int
+    users: dict[User, float]
+    title: str
+
+    def __init__(self, user_id: int, title: str):
+        self.user_id = user_id
+        self.title = title
+
+
+class RatingGraph:
+    """
+    A class for a recommender system that acts like a graph where users are linked to movies with
+    edges having the rating.
+
+    Private Instance Attributes:
+        -_users:
+            A mapping of users where the keys are the user_id and the values are the corresponding User Vertex.
+        -_movies:
+            A mapping of movies where the keys are the movie_id and the values are the corresponding Movie Vertex.
+
+    Representation Invariants:
+    ...
+    """
+    _users: dict[int, User]
+    _movies: dict[int, Movie]
+
+    def __init__(self):
+        """initialize an empty RatingGraph"""
+        self._users = {}
+        self._movies = {}
+
+    def add_users(self, user_id: int) -> None:
+        ...
+
+    def add_movies(self, movie_id: int, title: str) -> None:
+        ...
+
+    def add_edge(self, user_id: int, movie_id: int, rating: float) -> None:
+        user = ...
+        movie = ...
+        user[movie] = rating
+        movie[user] = rating
+
+def create_graph(csv_file_user: csv, csv_file_movie: csv) -> RatingGraph:
+    """
+    1. We add all the movies to the graph (movies node)
+    2. Access the users dataset
+    3. If the user is not in the graph -> we add the users (user_id)
+    4. Add the edgge(user_id, movie_id, rating)
+    """
+    ...
+
+def compute_cosine_similarity(movie1: Movie, movie2: Movie, graph: RatingGraph) -> Optional[float]:
+    """return None if only 1 or 0 user watched movie1 and movie2"""
+    ...
+
+def arrange_cosine_similarities() -> list[tuple[float, str]]:
+    """ return list of tuple in the form (cosine similarity, movie title)
+    Arrange all the cosine similarities.
+    If cosine similarity is none do not add to the tuple."""
+    # graph = create_graph()
+    ...
+
+def recommendations(watched_movies: list[str]) -> list[str]:
+    ...
