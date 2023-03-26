7fe325b039b4ebda508ce28d8455e8e619478fce
elidle <81723007+elidle@users.noreply.github.com>
2023-03-26 17:17:25 -0400

    test
    test

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import math
+
 print("Hello World")
 for i in range(0, 5):
   print(i)
   
+def cosine_similarity(m1: list[int], m2: list[int]) -> int:
+  """Returns the cosine similarity value between two lists.
+  
+  Preconditions:
+  - len(m1) == len(m2)
+  """
+  dot = sum([m1[i] * m2[i] for i in range(0, len(m1))])
+  n1 = sum([m1[i] ** 2 for i in range(0, len(m1))]) ** 0.5
+  n2 = sum([m2[i] ** 2 for i in range(0, len(m2))]) ** 0.5
+  
+  return dot / (n1 * n2) 
