1daf8eaf3c1523344e24318d0cd9f4a1453f9d79
Christoffer Tan <103194438+ChristofferTan@users.noreply.github.com>
2023-03-29 22:36:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -249,22 +249,33 @@ def recommendations(watched_movies: list[str]) -> list[str]:
 
     similar_movies = []
 
-    i = 0
     while True:
         for watched_movie in watched_movies:
             watched_movie_node = movie_title_mapping[watched_movie]
-            similar_movies.extend((arrange_cosine_similarities(watched_movie_node, graph, True))[i:i + 1])
+            recommended_movies = arrange_cosine_similarities(watched_movie_node, graph, True)
+
+            i = 0
+            while i < len(recommended_movies):
+                if not ((recommended_movies[i][1] in similar_movies) or (recommended_movies[i][1] in watched_movies)):
+                    similar_movies.append(recommended_movies[i][1])
+
+            i += 1
+
 
         if len(similar_movies) < 3:
-            similar_movies = []
             for watched_movie in watched_movies:
                 watched_movie_node = movie_title_mapping[watched_movie]
-                similar_movies.extend((arrange_cosine_similarities(watched_movie_node, graph, False))[i:i + 1])
+                recommended_movies = arrange_cosine_similarities(watched_movie_node, graph, False)
 
-        if valid(similar_movies, watched_movies):
-            return [movie[1] for movie in similar_movies]
+                i = 0
+                while i < len(recommended_movies):
+                    if not ((recommended_movies[i][1] in similar_movies) or (
+                            recommended_movies[i][1] in watched_movies)):
+                        similar_movies.append(recommended_movies[i][1])
 
-        i += 1
+                i += 1
+
+        return similar_movies
 
 
 def valid(chosen_movies: list[tuple[float, str]], watched_movies: list[str]) -> bool:
